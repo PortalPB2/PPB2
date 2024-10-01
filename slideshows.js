dc9f5457068ed49fed04d0acd9d3287d2c3903e5
Andrew Yaros <aeyaros@gmail.com>
2024-09-30 20:43:51 -0400

    formatting improvements

--- slideshows.js
+++ slideshows.js
@@ -172,11 +172,11 @@ class Slideshow {
 		
 		
 		const buttonSet = document.createElement('div'),
-			buttonSetPaddingL = document.createElement('div'),
+			// buttonSetPaddingL = document.createElement('div'),
 			buttonSetPaddingR = document.createElement('div');
 		
 		buttonSet.setAttribute('class','buttonSet');
-		buttonSetPaddingL.setAttribute('class','buttonSetPadding');
+		// buttonSetPaddingL.setAttribute('class','buttonSetPadding');
 		buttonSetPaddingR.setAttribute('class','buttonSetPadding buttonSetPaddingWithLine');
 		
 		const fs = document.createElement('div'),
@@ -194,7 +194,7 @@ class Slideshow {
 		this.#slideshowImageDiv.addEventListener('contextmenu', e => e.preventDefault(), { passive: false});
 		
 		buttonSet.append(prev, next, fs);
-		buttonContainer.append(buttonSetPaddingL, buttonSet, buttonSetPaddingR);
+		buttonContainer.append(/*buttonSetPaddingL,*/ buttonSet, buttonSetPaddingR);
 		this.#element.append(this.#slideshowImageDiv, buttonContainer);
 		
 		this.#currentSlide = this.#clamp(Math.floor(Math.random() * this.#images.length));
